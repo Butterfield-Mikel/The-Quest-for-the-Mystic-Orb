d957a033ad04a0db44f4110efa1a7921c17d89d0
Mikel <Mikel@Mikel-PC>
2015-10-06 19:44:56 -0600

    updated Player class.

--- QuestForTheMysticOrb/src/questforthemysticorb/QuestForTheMysticOrb.java
+++ QuestForTheMysticOrb/src/questforthemysticorb/QuestForTheMysticOrb.java
@@ -5,6 +5,8 @@
  */
 package questforthemysticorb;
 
+import model.Player;
+
 /**
  *
  * @author Mikel
@@ -15,8 +17,8 @@ public class QuestForTheMysticOrb {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("This works?");
+        
+        Player playerOne = new Player();
     }
     
 }
