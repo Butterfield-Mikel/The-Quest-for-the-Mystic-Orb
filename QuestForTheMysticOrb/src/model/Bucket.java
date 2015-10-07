64fbbde51d7ea8c71ebe41a9638b03060e470314
Mikel <Mikel@Mikel-PC>
2015-10-07 15:09:49 -0600

    updated Player class.

---
+++ QuestForTheMysticOrb/src/model/Bucket.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author Mikel
+ */
+public class Bucket implements Serializable {
+    
+    private String name;
+    private String water;
+
+    @Override
+    public String toString() {
+        return "Bucket{" + "name=" + name + ", water=" + water + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + Objects.hashCode(this.name);
+        hash = 89 * hash + Objects.hashCode(this.water);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Bucket other = (Bucket) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.water, other.water)) {
+            return false;
+        }
+        return true;
+    }
+
+    public Bucket() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWater() {
+        return water;
+    }
+
+    public void setWater(String water) {
+        this.water = water;
+    }
+}
