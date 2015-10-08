e2ae9deced10d476570fca06faca95904b27845e
Mikel <Mikel@Mikel-PC>
2015-10-08 13:09:18 -0600

    updated Player class.

--- QuestForTheMysticOrb/src/model/Location.java
+++ QuestForTheMysticOrb/src/model/Location.java
@@ -16,13 +16,19 @@ public class Location implements Serializable {
 
     private String row;
     private String column;
-    
-    
+    private Item item; 
+
+    @Override
+    public String toString() {
+        return "Location{" + "row=" + row + ", column=" + column + ", item=" + item + '}';
+    }
+
     @Override
     public int hashCode() {
-        int hash = 5;
-        hash = 59 * hash + Objects.hashCode(this.row);
-        hash = 59 * hash + Objects.hashCode(this.column);
+        int hash = 3;
+        hash = 37 * hash + Objects.hashCode(this.row);
+        hash = 37 * hash + Objects.hashCode(this.column);
+        hash = 37 * hash + Objects.hashCode(this.item);
         return hash;
     }
 
@@ -35,23 +41,21 @@ public class Location implements Serializable {
             return false;
         }
         final Location other = (Location) obj;
+        if (!Objects.equals(this.row, other.row)) {
+            return false;
+        }
+        if (!Objects.equals(this.column, other.column)) {
+            return false;
+        }
+        if (!Objects.equals(this.item, other.item)) {
+            return false;
+        }
         return true;
     }
 
-    @Override
-    public String toString() {
-        return "Location{" + "row=" + row + ", column=" + column + '}';
-    }
-
-   
-    
-    
     public Location() {
     }
-    
-    
-    
-    
+
     public String getRow() {
         return row;
     }
@@ -67,6 +71,15 @@ public class Location implements Serializable {
     public void setColumn(String column) {
         this.column = column;
     }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+  
      
     
 }
