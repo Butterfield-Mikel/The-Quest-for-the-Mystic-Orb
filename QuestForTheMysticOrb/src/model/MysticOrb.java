a28972f3e1d0a2a6d006afa406abf91fafee920e
Rich <Rich@Rich-PC.PK5001Z>
2015-10-09 16:12:52 -0600

    Updated orb

--- QuestForTheMysticOrb/src/model/MysticOrb.java
+++ QuestForTheMysticOrb/src/model/MysticOrb.java
@@ -6,6 +6,7 @@
 package model;
 
 import java.io.Serializable;
+import java.util.Objects;
 /**
  *
  * @author Rich
@@ -16,7 +17,8 @@ public class MysticOrb implements Serializable{
     }
     
     private String name;
-    
+    private Location location;
+
     public String getName() {
         return name;
     }
@@ -24,4 +26,47 @@ public class MysticOrb implements Serializable{
     public void setName(String name) {
         this.name = name;
     }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "MysticOrb{" + "name=" + name + ", location=" + location + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + Objects.hashCode(this.name);
+        hash = 29 * hash + Objects.hashCode(this.location);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final MysticOrb other = (MysticOrb) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.location, other.location)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
 }
