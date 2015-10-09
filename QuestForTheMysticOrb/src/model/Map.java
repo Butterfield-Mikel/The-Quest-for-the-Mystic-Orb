7ab52ff48f6a837c4e8f0fb2e63d3db2f4360adf
Rich <Rich@Rich-PC.PK5001Z>
2015-10-09 16:13:02 -0600

    Updated map

--- QuestForTheMysticOrb/src/model/Map.java
+++ QuestForTheMysticOrb/src/model/Map.java
@@ -16,22 +16,55 @@ public class Map implements Serializable{
     public Map (){
     }
     
-    private double rowCount;
-    private double columnCount;
+    private int row;
+    private int col;
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    @Override
+    public String toString() {
+        return "Map{" + "row=" + row + ", col=" + col + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + this.row;
+        hash = 29 * hash + this.col;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Map other = (Map) obj;
+        if (this.row != other.row) {
+            return false;
+        }
+        if (this.col != other.col) {
+            return false;
+        }
+        return true;
+    }
     
-    public double getName() {
-        return rowCount;
-    }
-
-    public void setName(double rowCount) {
-        this.rowCount = rowCount;
-    }
-
-    public double getName2() {
-        return columnCount;
-    }
-
-    public void setName2(double columnCount) {
-        this.columnCount = columnCount;
-    }
+    
 }
