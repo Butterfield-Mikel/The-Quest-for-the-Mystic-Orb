4e85aec4dc3260f314950c3dd8999ac8eff994fd
mannj14 <mannj14@mannj14-System-Product-Name>
2015-10-27 19:51:12 -0600

    edited banners

--- QuestForTheMysticOrb/src/view/WelcomeView.java
+++ QuestForTheMysticOrb/src/view/WelcomeView.java
@@ -20,6 +20,13 @@ public class WelcomeView {
     public void displayBanner() {
         
         System.out.println("********************************");
+        System.out.println("The game will play as a text-based roleplaying game where you navigate from the castle, ");
+        System.out.println("through the kingdom, and out into the unchartered lands outside the kingdom.");
+        System.out.println("You will face challenges, collect items, use those items to get more items, and get lost a few times. ");
+        System.out.println("Once you find the Mystic Orb of Rejuvenation you will have to take it back to the castle where the king will thank you and Merlyn");
+        System.out.println("the powerful wizard, can use it to restore the kingdom to its prosperous state.");
+        System.out.println("********************************");
+        System.out.println("");
         
     }
     
@@ -43,8 +50,12 @@ public class WelcomeView {
     }
     
     public void displayPlayerNameBanner(Player p) {
-           System.out.println("Welcome " + p.getName() + ":");
-           System.out.println("Have fun playing!");
+           System.out.println("");
+           System.out.println( p.getName() + ", King Simon has summoned you, ");
+           System.out.println("the head knight of the round table, because a dark wizard has cursed the kingdom with a famine." );
+           System.out.println("The only way to restore the kingdom to its prosperous state is to go on a dangerous quest ");
+           System.out.println(" to the deep Westland Woods where you can find the Mystic Orb of Rejuvenation.");
+           
     }
 
 
